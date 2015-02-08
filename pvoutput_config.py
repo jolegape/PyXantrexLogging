dc05c4fb625397fb6a8273944aac7729148ef172
Gavin Willett <github@jolegape.com>
2015-02-08 11:23:09 +1000

    Update pvoutput_config.py

--- pvoutput_config.py
+++ pvoutput_config.py
@@ -4,5 +4,7 @@
 PVOutputURL = "http://pvoutput.org"
 StatusURL = PVOutputURL + "/service/r2/addstatus.jsp"
 OutputURL = PVOutputURL + "/service/r2/addoutput.jsp"
-APIKey = "e016b161b16650460c8add474741a032750cd15d"
-SystemID = "5426"
+APIKey = "########################################"
+SystemID = "####"
+
+#API and System ID can be found on the PVOutput account page - http://pvoutput.org/account.jsp 
